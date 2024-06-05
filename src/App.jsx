546f5580fbc76d1932603560b93ef8e2b9bdf356
amuni27 <amanuuka@gm1il.com>
2024-06-05 16:04:47 -0500

    first commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
     return (
         <>
-            <Router basename="/Jemaw_X_Project">
+            <Router>
                 <Routes>
                     <Route path="/" element={<Login/>}></Route>
                     <Route path="/home" element={<Home/>}></Route>
